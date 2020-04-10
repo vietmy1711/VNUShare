80191a7a663b67ff7aaa3dcebaec15dabe6b68d3
mm <zzvietmyzz@gmail.com>
2020-04-10 10:22:13 +0700

    Init

---
+++ VNUShare/Controller/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  VNUShare
+//
+//  Created by MM on 4/10/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
---
+++ VNUShare/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  VNUShare
+//
+//  Created by MM on 4/10/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
