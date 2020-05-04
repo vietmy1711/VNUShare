8ea7e18fe7d7e2109f0352599f881fcee2b92c78
mm <zzvietmyzz@gmail.com>
2020-05-04 15:19:04 +0700

    Full screen fix

--- VNUShare/Controller/HomeViewController.swift
+++ VNUShare/Controller/HomeViewController.swift
@@ -13,9 +13,9 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello")
+        self.modalPresentationStyle = .fullScreen
     }
 
-
+    
 }
 
