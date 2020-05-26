e34bb0dff832516390c1c9e47b23da0ad6bf172f
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-05-26 13:46:18 +0700

    Add gitignore

---
+++ VNUShare/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  VNUShare
+//
+//  Created by MM on 5/25/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import Foundation
---
+++ VNUShare/Controller/MapPickViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapPickViewController.swift
+//  VNUShare
+//
+//  Created by MM on 5/25/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class MapPickViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
